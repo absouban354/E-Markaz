eaa432d294130aec73b85707104e8c2f6569a14d
Mushfeeeq <mushfeeqbm@gmail.com>
2017-09-14 13:53:00 +0530

    Merge branch 'master' of https://github.com/absouban354/E-Markaz
    # Conflicts:
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/in/tomtontech/markaz/Activity/CharityActivity.java
+++ app/src/main/java/in/tomtontech/markaz/Activity/CharityActivity.java
@@ -1,7 +1,9 @@
 package in.tomtontech.markaz.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import in.tomtontech.markaz.R;
 
@@ -11,5 +13,15 @@ public class CharityActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_charity);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+  }
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+    if (id == android.R.id.home) {
+      finish();
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
   }
 }
