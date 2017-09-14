974d63ac3ba13336e376d81bbccb6d9970b83262
anbk77 <ahammednafihbk@gmail.com>
2017-09-14 14:32:13 +0530

    changes by nap,insitutionDetails fix

--- app/src/main/java/in/tomtontech/markaz/Personal/PhotoDetails.java
+++ app/src/main/java/in/tomtontech/markaz/Personal/PhotoDetails.java
@@ -31,106 +31,108 @@ import in.tomtontech.markaz.CustomFunction;
 import in.tomtontech.markaz.R;
 
 public class PhotoDetails extends AppCompatActivity {
-  protected Activity avt;
-  protected Context ctx;
-  String photoDetails_photoId;
-  TextView tvName,tvDescription;
-  ImageView iv;
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    Bundle extra = getIntent().getExtras();
-    if (extra != null) {
-      photoDetails_photoId = extra.getString("photo_id");
-    }
-    setContentView(R.layout.activity_photo_details);
-    tvName=(TextView)findViewById(R.id.photoDetails_name);
-    tvDescription=(TextView)findViewById(R.id.photoDetails_description);
-    iv=(ImageView)findViewById(R.id.photoDetails_image);
-    PhotoDetailsAsync pda = new PhotoDetailsAsync();
-    pda.execute();
-  }
-
-  public class PhotoDetailsAsync extends AsyncTask<String, Void, String> {
-    String result;
-    Bitmap bitmap;
+    protected Activity avt;
+    protected Context ctx;
+    String photoDetails_photoId;
+    TextView tvName, tvDescription;
+    ImageView iv;
 
     @Override
-    protected String doInBackground(String... strings) {
-      try {
-        String photoUrl = CustomFunction.URL_ADDR.concat("photo_details.php");
-        URL url = new URL(photoUrl);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        String data = URLEncoder.encode("photo_id", "UTF-8") + "=" + URLEncoder
-            .encode(photoDetails_photoId, "UTF-8");
-        conn.setDoOutput(true);
-        conn.setDoInput(true);
-        OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-        wr.write(data);
-        wr.flush();
-        InputStream in = new BufferedInputStream(conn.getInputStream());
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        StringBuilder sb = new StringBuilder();
-        String line;
-        Log.v("haa2", "line nte adk ethi");
-        try {
-          while ((line = reader.readLine()) != null) {
-            sb.append(line).append('\n');
-            Log.v("id2", "whilente ullil" + line);
-          }
-        } catch (IOException e) {
-          e.printStackTrace();
-          return "failed";
-        } finally {
-          try {
-            in.close();
-          } catch (IOException e) {
-            e.printStackTrace();
-            return "failed";
-          }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle extra = getIntent().getExtras();
+        if (extra != null) {
+            photoDetails_photoId = extra.getString("photo_id");
         }
-        result = sb.toString();
-        Log.v("HAHAHAdetails", result);
-      } catch (IOException e) {
-        e.printStackTrace();
-        return "failed";
-      }
-      try {
-        JSONObject jb = new JSONObject(result);
-        String jaImg1 = jb.getString("photo");
-        URL url2 = new URL(CustomFunctions.URL_ADDR.concat(jaImg1));
-        Log.v("pic", "url" + url2);
-        InputStream is = (InputStream) url2.getContent();
-        bitmap = BitmapFactory.decodeStream(is);
-
-      } catch (JSONException | IOException e) {
-        e.printStackTrace();
-      }
-      return result;
+        setContentView(R.layout.activity_photo_details);
+        ctx = getApplicationContext();
+        tvName = (TextView) findViewById(R.id.photoDetails_name);
+        tvDescription = (TextView) findViewById(R.id.photoDetails_description);
+        iv = (ImageView) findViewById(R.id.photoDetails_image);
+        PhotoDetailsAsync pda = new PhotoDetailsAsync();
+        pda.execute();
     }
 
-    @Override
-    protected void onPostExecute(String result) {
-      if (result.equalsIgnoreCase("failed")) {
+    public class PhotoDetailsAsync extends AsyncTask<String, Void, String> {
+        String result;
+        Bitmap bitmap;
 
-        Toast.makeText(ctx, "Network Error. Please turn on Mobile data or WiFi and try again",
-            Toast.LENGTH_SHORT).show();
-      } else {
-        try {
-          String photoDetails_name, photoDetails_date, photoDetails_description;
-          JSONObject jb = new JSONObject(result);
-          photoDetails_name=jb.getString("photo_name");
-          photoDetails_date=jb.getString("photo_date");
-          photoDetails_description=jb.getString("photo_description");
-          tvName.setText(getString(R.string.photoDetails_name,photoDetails_name,photoDetails_date));
-          tvDescription.setText(photoDetails_description);
-          iv.setImageBitmap(bitmap);
-          iv.setScaleType(ImageView.ScaleType.FIT_XY);
-        } catch (JSONException e) {
-          e.printStackTrace();
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                String photoUrl = CustomFunctions.URL_ADDR.concat("photo_details.php");
+                URL url = new URL(photoUrl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                String data = URLEncoder.encode("photo_id", "UTF-8") + "=" + URLEncoder
+                        .encode(photoDetails_photoId, "UTF-8");
+                conn.setDoOutput(true);
+                conn.setDoInput(true);
+                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+                wr.write(data);
+                wr.flush();
+                InputStream in = new BufferedInputStream(conn.getInputStream());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                StringBuilder sb = new StringBuilder();
+                String line;
+                Log.v("haa2", "line nte adk ethi");
+                try {
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line).append('\n');
+                        Log.v("id2", "whilente ullil" + line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return "failed";
+                } finally {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return "failed";
+                    }
+                }
+                result = sb.toString();
+                Log.v("HAHAHAdetails", result);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "failed";
+            }
+            try {
+                JSONObject jb = new JSONObject(result);
+                String jaImg1 = jb.getString("photo");
+                URL url2 = new URL(CustomFunctions.URL_ADDR.concat(jaImg1));
+                Log.v("pic", "url" + url2);
+                InputStream is = (InputStream) url2.getContent();
+                bitmap = BitmapFactory.decodeStream(is);
+
+            } catch (JSONException | IOException e) {
+                e.printStackTrace();
+            }
+            return result;
         }
-      }
 
+        @Override
+        protected void onPostExecute(String result) {
+            if (result.equalsIgnoreCase("failed")) {
+
+                Toast.makeText(ctx, "Network Error. Please turn on Mobile data or WiFi and try again", Toast.LENGTH_SHORT).show();
+                finish();
+            } else {
+                try {
+                    String photoDetails_name, photoDetails_date, photoDetails_description;
+                    JSONObject jb = new JSONObject(result);
+                    photoDetails_name = jb.getString("photo_name");
+                    photoDetails_date = jb.getString("photo_date");
+                    photoDetails_description = jb.getString("photo_description");
+                    tvName.setText(getString(R.string.photoDetails_name, photoDetails_name, photoDetails_date));
+                    tvDescription.setText(photoDetails_description);
+                    iv.setImageBitmap(bitmap);
+                    iv.setScaleType(ImageView.ScaleType.FIT_XY);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
     }
-  }
 }
